23f7068c8723fd0334daabbb58ca28e24d8893a9
Naeem Sajjad <naeem.sajjad@nxb.com.pk>
2026-08-17 18:03:22 +0500

    Sorted: sorted array using bubble sort

---
+++ Arrays/Bubble_Sort.js
@@ -0,0 +1,9 @@
+let arr=[1,9,2,8,3,5]
+for (let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length-1-i;j++){
+        if(arr[j]>arr[j+1]){
+            [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+        }
+    }
+}
+console.log(arr)
