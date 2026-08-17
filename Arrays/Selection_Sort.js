2f24de64744a0a5040417be3273bb25ac064f4e9
Naeem Sajjad <naeem.sajjad@nxb.com.pk>
2026-08-17 18:20:44 +0500

    Solved: Selection sort on array

---
+++ Arrays/Selection_Sort.js
@@ -0,0 +1,12 @@
+let arr=[2,4,1,5,9,8,-2]
+
+for (let i=0; i<arr.length-1;i++){
+    let minIndex=i
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[j]<arr[minIndex]){
+            minIndex=j
+        }
+    }
+    [arr[i],arr[minIndex]]=[arr[minIndex],arr[i]]
+}
+console.log(arr)
