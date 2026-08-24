17b622b2a6b5e6bb99c6593d49c86e2a552016bf
Naeem Sajjad <naeem.sajjad@nxb.com.pk>
2026-08-24 15:27:39 +0500

    Fixed: resolved exceptional case of empty string

--- Strings/Longest_SubString_in_String.js
+++ Strings/Longest_SubString_in_String.js
@@ -1,4 +1,8 @@
 let str="i am naeem sajjad which is following the string concept"
+if(str.trim()===""){
+    console.log("Empty string not allowed");
+    return
+}
 let arr=str.split(" ")
 let longest=arr[0]
 for (let item of arr){
