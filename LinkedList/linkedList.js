e9ab1797b590d1b944e19cdd33ab13c3e17d76e3
Naeem Sajjad <naeem.sajjad@nxb.com.pk>
2026-08-25 13:23:47 +0500

    Build: A basic version of linked List by creating node and list and function to add nodes

---
+++ LinkedList/linkedList.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+}
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+
