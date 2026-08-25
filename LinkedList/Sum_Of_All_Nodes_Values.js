1e941a098ced7a9964ff79f1a01c6a350932dff0
Naeem Sajjad <naeem.sajjad@nxb.com.pk>
2026-08-25 13:27:25 +0500

    Solved: Sum of all values of nodes in a linkedList

---
+++ LinkedList/Sum_Of_All_Nodes_Values.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+}
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+
+
+
+function Sum_of_Values(head){
+    let current =head;
+    let sum=0
+    while(current!==null){
+        sum+=current.value;
+        current=current.next
+    }
+    return sum
+}
+
+console.log(Sum_of_Values(list.head))
