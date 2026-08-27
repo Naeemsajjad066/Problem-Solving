333f6399ef95e1e4ab04d742137c05c0b77991e1
Naeem Sajjad <naeem.sajjad@nxb.com.pk>
2026-08-27 13:27:11 +0500

    Solved: Delete Node at start problem

---
+++ LinkedList/Delete_First_Node.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    traverse(){
+        let current=this.head
+        while(current!==null){
+            console.log(current.value," ")
+            current=current.next
+        }
+    }
+}
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+
+function DeleteFirstNode(head){ 
+    if( head===null) return null;
+    console.log(head);
+    head=head.next
+    console.log(head)
+    return head
+
+}
+
+list.head=DeleteFirstNode(list.head)
+list.traverse()
