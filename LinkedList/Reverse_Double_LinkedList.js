b35832fac4c087a07bd67f401564a27c68daa1fd
Naeem Sajjad <naeem.sajjad@nxb.com.pk>
2026-08-31 17:35:24 +0500

    Solved: Reverse doubly linkedList problem using array destructuring

---
+++ LinkedList/Reverse_Double_LinkedList.js
@@ -0,0 +1,56 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+    append(value) {
+        const newNode = new Node(value)
+        if (this.head === null) {
+            this.head = newNode
+            this.tail = newNode
+            return
+        }
+        this.tail.next = newNode
+        newNode.prev = this.tail
+        this.tail = newNode
+    }
+    traverse() {
+        if (this.head === null) return this.head
+        let current = this.head
+        while (current !== null) {
+            console.log(current.value);
+            current = current.next
+
+        }
+        console.log("\n");
+    }
+    reverseDoubly() {
+     let curr=this.head
+     if(this.head===null) return null
+     while(curr!==null){
+        [curr.next,curr.prev]=[curr.prev,curr.next]
+        curr=curr.prev
+     }
+    [this.tail,this.head]=[this.head,this.tail]
+    }
+}
+
+const list = new LinkedList()
+list.append(10)
+list.append(20)
+list.append(30)
+
+list.traverse()
+
+console.log("Reverse Doubly");
+list.reverseDoubly()
+
+list.traverse()
